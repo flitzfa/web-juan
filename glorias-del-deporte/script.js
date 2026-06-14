639a6e841a519dbc0fd823a45dd19c2a718b9c19
flitzfa <fabrix.enriquez@gmail.com>
2026-06-14 17:48:33 -0300

    actualizar enlaces de audio para "El Pato Fillol" y "Santiago Ponzinibbio"

--- glorias-del-deporte/script.js
+++ glorias-del-deporte/script.js
@@ -7,7 +7,7 @@ const tracks = [
   {
     title: "El Pato Fillol",
     image: "images/el-pato-fillol.png",
-    audio: "https://www.dropbox.com/scl/fi/vmig6rxi1q780fsfxoiuw/TRACK_02_PatoFillol.mp3?rlkey=omqnxob5hw23wxmo7dw4pl7cr&st=3wiqyros&raw=1",
+    audio: "https://www.dropbox.com/scl/fi/xn2q0br7s516g0wwu2bwk/TRACK_02_PatoFillol.mp3?rlkey=vy9ekov85otp1rgow7wllndu0&st=bw06it80&raw=1",
   },
   {
     title: "Esteban Ribovics",
@@ -37,7 +37,7 @@ const tracks = [
   {
     title: "Santiago Ponzinibbio",
     image: "images/santiago-ponzinibbio.png",
-    audio: "https://www.dropbox.com/scl/fi/iri9v0msr2m65h5r17p4d/TRACK_08_SantiagoPonzinibbio.mp3?rlkey=o8loi9h7b5mgpjww8af9qhz8v&st=hm8fazdg&raw=1",
+    audio: "https://www.dropbox.com/scl/fi/18wcl03bzixqiddfn6bdl/TRACK_08_SantiagoPonzinibbio.mp3?rlkey=ppixw9fzzgirg9kxer4e123eg&amp;st=qyjxsoj3&amp;raw=1",
   },
 ];
 
